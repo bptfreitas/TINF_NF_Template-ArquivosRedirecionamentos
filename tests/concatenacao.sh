f7c55566af15b7b607de70ae8f3fb36e25dfd399
Bruno <bruno.freitas@cefet-rj.br>
2024-07-26 14:55:41 -0300

    Correção de bugs

--- tests/concatenacao.sh
+++ tests/concatenacao.sh
@@ -2,15 +2,15 @@
 
 cd ..
 
-cat mensagem${1..3}.txt > ./.teste.txt
+cat mensagem1.txt mensagem2.txt mensagem3.txt > ./.teste.txt
 
 if [[ `diff -q ./mensagens.txt ./.teste.txt` == "" ]]; then 
 
-	return 0
+	exit 0
 
 else
 
-	return 1
+	exit 1
 
 fi
 
