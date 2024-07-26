5400e66adcb38135fa22f2616d83ccbff4b99340
Bruno <bruno.freitas@cefet-rj.br>
2024-07-26 15:26:01 -0300

    Remoção de bugs

--- tests/quantidade_linhas.sh
+++ tests/quantidade_linhas.sh
@@ -5,11 +5,17 @@ test_file="./teste_qtd_linhas.txt"
 
 cd ..
 
+if [[ ! -f linhas.txt ]]; then
+
+	exit 1
+	
+fi
+
 wc -l mensagens.txt | cut -f1 -d" " > ${test_file}
 
 nr_esperado=`grep -E -o "[0-9]+" ${test_file}`
 
-nr_obtido=`grep -E -o "[0-9]+" mensagens.txt`
+nr_obtido=`grep -E -o "[0-9]+" linhas.txt`
 
 if [[ $nr_obtido -eq $nr_esperado ]]; then 
 
