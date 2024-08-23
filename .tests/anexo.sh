64ac537ef5f9d9787340ea993a11d967ca3cae07
Bruno <bruno.freitas@cefet-rj.br>
2024-08-23 12:10:26 -0300

    anexo.sh: added existence test

--- .tests/anexo.sh
+++ .tests/anexo.sh
@@ -3,6 +3,20 @@
 TEST_FILE="./mensagens_antiga.txt"
 CHECK_FILE="./mensagens.txt"
 
+if [[ ! -f "${TEST_FILE}" ]]; then
+
+	exit 1;
+
+fi
+
+if [[ ! -f "${CHECK_FILE}" ]]; then
+
+	exit 1;
+
+fi
+
+
+
 nlines_test=`wc -l $TEST_FILE | cut -f1 -d " "`
 
 echo $nlines_test
