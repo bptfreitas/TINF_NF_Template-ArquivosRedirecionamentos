c7028c5028428f0fcfb775730012cbc982537f22
Bruno <bruno.freitas@cefet-rj.br>
2024-08-23 12:20:44 -0300

    saida_com_erros.sh: added file existence check

--- .tests/saida_com_erros.sh
+++ .tests/saida_com_erros.sh
@@ -2,6 +2,12 @@
 
 ls -R /etc 1> teste_saida_com_erros.txt 2>&1
 
+
+if [[ ! -f saida_com_erros.txt ]]; then 
+	echo "saida_com_erros.txt nao existe!"
+	exit 1
+fi
+
 if [[ `diff -q saida_com_erros.txt teste_saida_com_erros.txt` != "" ]]; then
 
 	exit 1
