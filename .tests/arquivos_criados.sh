ef7b0ab3d7cb9c500da2130dab955faef99676c8
Bruno <bruno.freitas@cefet-rj.br>
2024-10-22 15:53:26 -0300

    Corrected bug on return code when wrong execution

--- .tests/arquivos_criados.sh
+++ .tests/arquivos_criados.sh
@@ -5,8 +5,8 @@ for index in $(seq 1 3); do
 	if [[ ! -f mensagem${index}.txt ]]; then 
 	
 		echo "ERRO: mensagem${index}.txt nao criado!"
-	
-		exit ${i}
+					
+		exit ${index}
 		
 	fi
 	
@@ -14,7 +14,7 @@ for index in $(seq 1 3); do
 	
 		echo "ERRO: mensagem${index}.txt eh um arquivo vazio!"
 	
-		exit $(( i*10 ))
+		exit $(( index*10 ))
 		
 	fi	
 
