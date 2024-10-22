fe8fa620f38dcfe10a4d3d6fa8d88c08572c3f4d
Bruno <bruno.freitas@cefet-rj.br>
2024-10-22 15:31:09 -0300

    Added empty files check

--- .tests/arquivos_criados.sh
+++ .tests/arquivos_criados.sh
@@ -9,9 +9,17 @@ for index in $(seq 1 3); do
 		exit ${i}
 		
 	fi
+	
+	if [[ ! -s mensagem${index}.txt ]]; then 
+	
+		echo "ERRO: mensagem${index}.txt eh um arquivo vazio!"
+	
+		exit $(( i*10 ))
+		
+	fi	
 
 done
 
-# arquivos existem
+# arquivos existem e nao sao vazios
 
 exit 0
