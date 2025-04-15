f149b3e7347959e96853eb2ec6c7e3ee70833cdf
Bruno Policarpo <bruno.freitas@cefet-rj.br>
2025-04-15 13:32:41 -0300

    Updating grade_student.sh to new format

--- .grade_student.sh
+++ .grade_student.sh
@@ -1,5 +1,76 @@
 #!/bin/bash
 
-./trabalho.sh
+points=0
+total=0
 
-./corretor
+echo "QUESTAO 1"
+./questao01.sh
+./.tests/fortune.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 2"
+./questao02.sh
+./.tests/arquivos_criados.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 3"
+./questao03.sh
+./.tests/concatenacao.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 4"
+./questao04.sh
+./.tests/anexo.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo 
+
+echo "QUESTAO 5"
+./questao05.sh
+./.tests/quantidade_linhas.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo 
+
+echo "QUESTAO 6"
+./questao06.sh
+./.tests/quantidade_letras.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 7"
+./questao07.sh
+./.tests/conteudo_etc.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 8"
+./questao08.sh
+./.tests/saida_com_erros.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 9"
+./questao09.sh
+./.tests/usuarios.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "QUESTAO 10"
+./questao10.sh
+./.tests/usuarios_ordenados.sh
+[[ $? -eq 0 ]] && points=$(( points + 1 ))
+total=$(( total + 1))
+echo
+
+echo "NOTA: ${points}/${total}"
